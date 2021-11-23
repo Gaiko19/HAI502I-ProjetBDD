adf39ff3a0aa3fd3d8e5985598c416dfe87a9634
Gaiko19 <47215835+Gaiko19@users.noreply.github.com>
2021-11-23 16:20:13 +0000

    Initial commit

---
+++ creationVillage.sql
@@ -0,0 +1,41 @@
+CREATE TABLE Village (
+	idVillage INT(10) NOT NULL, 
+  nomJoueur VARCHAR(20) NOT NULL,
+  niveauJoueur,
+  idClan,
+  gradeClan,
+  planVillage
+  CONSTRAINT PK_IDVILLLAGE PRIMARY KEY idVillage
+);
+
+CREATE TABLE Clan(
+
+)
+
+CREATE TABLE (
+
+)
+
+CREATE TABLE (
+
+)
+
+CREATE TABLE (
+
+)
+
+CREATE TABLE (
+
+)
+
+CREATE TABLE (
+
+)
+
+CREATE TABLE (
+
+)
+
+CREATE TABLE (
+
+)
