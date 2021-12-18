bd633368e294fd93a62a18804b43b06d8d53c82d
Arnaud <61588785+ArnaudCs@users.noreply.github.com>
2021-12-18 10:13:06 +0000

    modification de requêtes

--- requetes.sql
+++ requetes.sql
@@ -1,25 +1,25 @@
 
---Toutes les troupes différentes du village de 'MateoDu13' (Requete Group By)
+prompt -Toutes les troupes différentes du village de 'MateoDu13' (Requete Group By)
 SELECT Troupe.nomTroupe FROM Troupe, Camp, Village
-WHERE Troupe.idTroupe=Camp.typeTroupe
+WHERE Troupe.idTroupe=Camp.idTroupe
 AND Camp.idVillage=Village.idVillage
 AND Village.nomJoueur='MATEODU13'
 GROUP BY Troupe.nomTroupe;
 
---Or Moyen du clan GNUMZ
+prompt -Or Moyen du clan GNUMZ
 SELECT AVG(quantite) FROM Reserve, Village, Clan
 AND Reserve.typeReserve='OR'
 AND Reserve.idVillage=Village.idVillage
 AND Village.idClan=Clan.idClan
 AND Clan.nomClan='GNUMZ'
 
---Nombre de troupes du village de 'Chumm' (group by x2)
+prompt -Nombre de troupes du village de 'Chumm' (group by x2)
 SELECT Village.nomJoueur, COUNT(*) FROM Camp, Village
 AND Camp.idVillage=Village.idVillage
 AND Village.nomJoueur='DOOBY'
 GROUP BY Village.nomJoueur;
 
--- Requete avec division : Existe-t-il un village tel qu'il n'existe aucune troupe qui ne soit pas formé par ce village ?
+prompt -Requete avec division : Existe-t-il un village tel qu'il n'existe aucune troupe qui ne soit pas formé par ce village ?
 SELECT * FROM Village
   WHERE NOT EXISTS
     (SELECT * FROM Troupe WHERE NOT EXISTS
@@ -29,8 +29,8 @@ SELECT * FROM Village
 
 -- Des unités non utilisées dans le camp (Sous-requête)
 SELECT Troupe.idTroupe, Troupe.NomTroupe FROM Troupe 
-WHERE Troupe.idTroupe NOT IN (SELECT Camp.TypeTroupe FROM Camp 
-                            GROUP BY Camp.TypeTroupe);
+WHERE Troupe.idTroupe NOT IN (SELECT Camp.idTroupe FROM Camp 
+                            GROUP BY Camp.idTroupe);
 
 
 -- Requete avec ss requete corrélative : Quels sont les villages qui forment des troupes
