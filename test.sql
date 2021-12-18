6906109ee2a23678121f2d301ed510b289bc1239
Arnaud <61588785+ArnaudCs@users.noreply.github.com>
2021-12-18 14:33:41 +0000

    modif de changement

--- test.sql
+++ test.sql
@@ -20,7 +20,7 @@ prompt
 prompt "Création d'un village 'VAL' qui sera chef de clan"
 INSERT INTO Village(idVillage, nomJoueur, niveauJoueur,capaciteeCampMax, trophees, idClan) VALUES (200, 'VAL', 47, null, 1300, null);
 prompt "Création d'un clan 'TEST0' avec Val comme chef"
-INSERT INTO Clan VALUES (36,'TEST0','FR', 15, 120);
+INSERT INTO Clan VALUES (36,'TEST0','FR', 15, 200);
 prompt "Affichage du chef de clan"
 SELECT idChefDeClan, nomJoueur FROM Clan, Village WHERE Clan.idClan = Village.idClan AND Village.idClan = 36;
 
@@ -94,78 +94,6 @@ SELECT nbrTroupe, idTroupe FROM Camp where idVillage = 60 GROUP BY idTroupe, nbr
 
 prompt "Tentative d'insertion de 300 archères dans le village de martin"
 INSERT INTO Camp VALUES (38, 2, 60, 300);
-
-/*
---Test trigger rejoindre un clan s'il n'y a pas de place --INSERT INTO Clan VALUES (ID,Nom,region,niveau,chef) (BUG)
-prompt
-prompt "##########################################################"
-prompt "Test pour voir si il reste une place dans le Clan numéro 33"
-prompt "##########################################################"
-prompt 
-prompt "Creation d'un clan avec Martin comme chef"
-INSERT INTO Clan VALUES (33,'TEST','FR', 15, 60);
-
-prompt "Création de 49 membres (C'est /très/ long)"
-INSERT INTO Village(idVillage, nomJoueur, idClan) VALUES (100, '1', 33);
-INSERT INTO Village(idVillage, nomJoueur, idClan) VALUES (101, '2', 33);
-INSERT INTO Village(idVillage, nomJoueur, idClan) VALUES (102, '3', 33);
-INSERT INTO Village(idVillage, nomJoueur, idClan) VALUES (103, '4', 33);
-INSERT INTO Village(idVillage, nomJoueur, idClan) VALUES (104, '5', 33);
-INSERT INTO Village(idVillage, nomJoueur, idClan) VALUES (105, '6', 33);
-INSERT INTO Village(idVillage, nomJoueur, idClan) VALUES (106, '7', 33);
-INSERT INTO Village(idVillage, nomJoueur, idClan) VALUES (107, '8', 33);
-INSERT INTO Village(idVillage, nomJoueur, idClan) VALUES (108, '9', 33);
-INSERT INTO Village(idVillage, nomJoueur, idClan) VALUES (109, '10', 33);
-
-INSERT INTO Village(idVillage, nomJoueur, idClan) VALUES (110, '11', 33);
-INSERT INTO Village(idVillage, nomJoueur, idClan) VALUES (111, '12', 33);
-INSERT INTO Village(idVillage, nomJoueur, idClan) VALUES (112, '13', 33);
-INSERT INTO Village(idVillage, nomJoueur, idClan) VALUES (113, '14', 33);
-INSERT INTO Village(idVillage, nomJoueur, idClan) VALUES (114, '15', 33);
-INSERT INTO Village(idVillage, nomJoueur, idClan) VALUES (115, '16', 33);
-INSERT INTO Village(idVillage, nomJoueur, idClan) VALUES (116, '17', 33);
-INSERT INTO Village(idVillage, nomJoueur, idClan) VALUES (117, '18', 33);
-INSERT INTO Village(idVillage, nomJoueur, idClan) VALUES (118, '19', 33);
-INSERT INTO Village(idVillage, nomJoueur, idClan) VALUES (119, '20', 33);
-
-INSERT INTO Village(idVillage, nomJoueur, idClan) VALUES (120, '21', 33);
-INSERT INTO Village(idVillage, nomJoueur, idClan) VALUES (121, '22', 33);
-INSERT INTO Village(idVillage, nomJoueur, idClan) VALUES (122, '23', 33);
-INSERT INTO Village(idVillage, nomJoueur, idClan) VALUES (123, '24', 33);
-INSERT INTO Village(idVillage, nomJoueur, idClan) VALUES (124, '25', 33);
-INSERT INTO Village(idVillage, nomJoueur, idClan) VALUES (125, '26', 33);
-INSERT INTO Village(idVillage, nomJoueur, idClan) VALUES (126, '27', 33);
-INSERT INTO Village(idVillage, nomJoueur, idClan) VALUES (127, '28', 33);
-INSERT INTO Village(idVillage, nomJoueur, idClan) VALUES (128, '29', 33);
-INSERT INTO Village(idVillage, nomJoueur, idClan) VALUES (129, '30', 33);
-
-INSERT INTO Village(idVillage, nomJoueur, idClan) VALUES (130, '31', 33);
-INSERT INTO Village(idVillage, nomJoueur, idClan) VALUES (131, '32', 33);
-INSERT INTO Village(idVillage, nomJoueur, idClan) VALUES (132, '33', 33);
-INSERT INTO Village(idVillage, nomJoueur, idClan) VALUES (133, '34', 33);
-INSERT INTO Village(idVillage, nomJoueur, idClan) VALUES (134, '35', 33);
-INSERT INTO Village(idVillage, nomJoueur, idClan) VALUES (135, '36', 33);
-INSERT INTO Village(idVillage, nomJoueur, idClan) VALUES (136, '37', 33);
-INSERT INTO Village(idVillage, nomJoueur, idClan) VALUES (137, '38', 33);
-INSERT INTO Village(idVillage, nomJoueur, idClan) VALUES (138, '39', 33);
-INSERT INTO Village(idVillage, nomJoueur, idClan) VALUES (139, '40', 33);
-
-INSERT INTO Village(idVillage, nomJoueur, idClan) VALUES (140, '41', 33);
-INSERT INTO Village(idVillage, nomJoueur, idClan) VALUES (141, '42', 33);
-INSERT INTO Village(idVillage, nomJoueur, idClan) VALUES (142, '43', 33);
-INSERT INTO Village(idVillage, nomJoueur, idClan) VALUES (143, '44', 33);
-INSERT INTO Village(idVillage, nomJoueur, idClan) VALUES (144, '45', 33);
-INSERT INTO Village(idVillage, nomJoueur, idClan) VALUES (145, '46', 33);
-INSERT INTO Village(idVillage, nomJoueur, idClan) VALUES (146, '47', 33);
-INSERT INTO Village(idVillage, nomJoueur, idClan) VALUES (147, '48', 33);
-
-prompt "Insertion du 50eme membres "
-INSERT INTO Village(idVillage, nomJoueur, idClan) VALUES (148, '49', 33);
-
-prompt "Tentative d'insertion d'un 51eme membre"
-INSERT INTO Village(idVillage, nomJoueur, idClan) VALUES (149, '50', 33);
-*/
-
 
 --Test trigger passage des reserves en négatif à 0 (fonctionnel)
 prompt
